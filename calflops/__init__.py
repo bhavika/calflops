6e855ce2519f775f81c7a33062fec5f09f21334f
bhavika <bhavicka.7992@gmail.com>
2024-06-27 16:15:06 -0400

    Imports

--- calflops/__init__.py
+++ calflops/__init__.py
@@ -1,3 +1,15 @@
 # !usr/bin/env python
 # -*- coding:utf-8 -*-
 
+
+from .flops_counter import calculate_flops
+from .flops_counter_hf import calculate_flops_hf
+
+from .utils import generate_transformer_input
+from .utils import number_to_string
+from .utils import flops_to_string
+from .utils import macs_to_string
+from .utils import params_to_string
+from .utils import bytes_to_string
+
+from .estimate import create_empty_model
